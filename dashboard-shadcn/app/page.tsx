f660fd54b2a5b9d71ce09b16f4c160495efb2329
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2025-06-05 17:56:37 +0000

    feat: estruturacao do projeto - adicionando a sidebar ao layout 1

---
+++ dashboard-shadcn/app/page.tsx
@@ -0,0 +1,33 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-arrea-interactive"
+import { SectionCards } from "@/components/section-cards"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
+  return (
+   <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              {/* <DataTable data={data} /> */}
+            </div>
+          </div>
+      </div>
+  )
+}
